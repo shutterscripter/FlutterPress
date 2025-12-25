d4d36b43ada19f045be8f8e65966d4af74ae183f
shutterscripter <jay2272001@gmail.com>
2025-12-25 23:10:25 +0530

    Supabase Implemented for auth

--- frontend/lib/constants/api_constants.dart
+++ frontend/lib/constants/api_constants.dart
@@ -1,6 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'http://103.150.186.252:1823';
-  // static String baseUrl = 'http://0.0.0.0:8000';
+  static String baseUrl = 'http://0.0.0.0:8000';
   static String register = '$baseUrl/auth/register';
   static String login = '$baseUrl/auth/token';
   static String summarizeNews = '$baseUrl/news/summarize';
